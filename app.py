73c6dab5fd6200412f9efd605f2faa9a4b8292a4
Sunthunder0813 <santanderjoseph13@gmail.com>
2025-12-25 19:32:14 +0800

    debug port

--- app.py
+++ app.py
@@ -444,4 +444,5 @@ def api_zone_selector():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
